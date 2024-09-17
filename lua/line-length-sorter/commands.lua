4594bd119717d3df837e5d70330ad644b8cea56f
cqb13 <cqb13.main@gmail.com>
2024-09-16 22:55:50 -0400

    fixed loading

--- lua/line-length-sorter/commands.lua
+++ lua/line-length-sorter/commands.lua
@@ -1,19 +1,25 @@
 local sorter = require("line-length-sorter.sorter")
 
+local function parse_args(args)
+	local remove_blank = false
+	if args and args:match("remove_blank=true") then
+		remove_blank = true
+	end
+	return remove_blank
+end
+
 local function sort_longest_to_shortest_cmd(opts)
-	local remove_blank = opts.args == "true"
+	local remove_blank = parse_args(opts.args)
 	sorter.sort_longest_to_shortest(remove_blank)
 end
 
 local function sort_shortest_to_longest_cmd(opts)
-	local remove_blank = opts.args == "true"
+	local remove_blank = parse_args(opts.args)
 	sorter.sort_shortest_to_longest(remove_blank)
 end
 
--- Define commands
-vim.api.nvim_create_user_command("SortLongestToShortest", sort_longest_to_shortest_cmd, { nargs = "?" })
-
-vim.api.nvim_create_user_command("SortShortestToLongest", sort_shortest_to_longest_cmd, { nargs = "?" })
+vim.api.nvim_create_user_command("SortLongestToShortest", sort_longest_to_shortest_cmd, { nargs = "*" })
+vim.api.nvim_create_user_command("SortShortestToLongest", sort_shortest_to_longest_cmd, { nargs = "*" })
 
 return {
 	sort_longest_to_shortest_cmd = sort_longest_to_shortest_cmd,
